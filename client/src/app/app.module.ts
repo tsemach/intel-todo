03b846de6f9351582b5eb01145d9b949bda4b56b
tsemach <tsemach.mizrachi>
2019-08-01 00:58:32 +0300

    need to commit cause update

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToDosComponent } from './todos/todos.component';
+import { ToDoListComponent } from './todolist/todolist.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDosComponent,
+    ToDoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FlexLayoutModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
