fe34cfe19b8c122b872756696b127ccb8281abf9
tsemach <tsemach.mizrachi>
2019-08-02 14:07:21 +0300

    add item list items

--- client/src/app/todo-list/todo-list-head/todo-list-head.component.ts
+++ client/src/app/todo-list/todo-list-head/todo-list-head.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TodoListHeadComponent implements OnInit {
   @Input() title: string;
+  @Input() count: string;
 
   constructor() { }
 
--- client/src/app/todo-list/todo-list-item/todo-list-item.component.ts
+++ client/src/app/todo-list/todo-list-item/todo-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {ToDoItemModel} from "../../common/todos.model";
 
 @Component({
   selector: 'app-todo-list-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list-item.component.scss']
 })
 export class TodoListItemComponent implements OnInit {
+  @Input() item: ToDoItemModel;
 
   constructor() { }
 
